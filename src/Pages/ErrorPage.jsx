03cec5602e57459299ca1abc357913da734f8b68
maowimim <maowa087@gmail.com>
2025-10-24 19:19:44 +0600

    added error page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-
+import React from 'react';
+import error from '../assets/error-404.png'
+import { Link } from 'react-router';
+import Navbar from '../Components/Header/Navbar';
+import Footer from '../Components/Footer/Footer';
 const ErrorPage = () => {
-  return (
-    <div>
-      <h1>404 not found</h1>
-    </div>
-  )
-}
+    return (
+        <div className='flex flex-col min-h-screen'>
+            <Navbar></Navbar>
+            <div className='flex-1'>
+                <div className='flex flex-col justify-center items-center mb-15'>
+                    <img src={error} alt="" />
+                    <div className='text-center space-y-2.5'>
+                        <h1 className='font-semibold text-5xl'>Oops, page not found!</h1>
+                        <p className='text-xl text-[#627382]'>The page you are looking for is not available.</p>
+                        <Link to='/apps'>
+                            <button className='py-4 px-8 rounded-sm text-white font-semibold bg-gradient-to-r from-[#632EE3] to-[#9F62F2]'>Go Back!</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
 
-export default ErrorPage
-
+export default ErrorPage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,10 +4,12 @@ import MainLayout from "../Layouts/MainLayout";
 import Apps from "../Pages/Apps";
 import Install from "../Pages/Install";
 import AppDetails from "../Pages/AppDetails";
+import ErrorPage from "../Pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
+        errorElement: <ErrorPage/>,
         element: <MainLayout />,
         hydrateFallbackElement: <p>Loading...</p>,
         children: [
